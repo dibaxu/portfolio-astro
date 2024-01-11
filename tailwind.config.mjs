93259fe15fd90ced00e15ad38654571d6cdad715
[Pedro Sota] <psptaier@gmail.com>
2024-01-11 19:43:54 -0300

    astro add React

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#89B9AD",
+        secondary: "#C7DCA7",
+        terciary: "#FFEBD8",
+      },
+    },
   },
   plugins: [],
 };
